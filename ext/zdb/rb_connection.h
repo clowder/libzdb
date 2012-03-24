76d37931ebaf17cec15705d1e6ed747c471f2cdd
Chris Lowder <clowder@gmail.com>
2012-03-24 10:40:32 +0000

    Adding copyright notices to to all the source files.

--- ext/zdb/rb_connection.h
+++ ext/zdb/rb_connection.h
@@ -1,3 +1,11 @@
+/*
+ * libzdb (http://github.com/clowder/libzdb)
+ * Ruby bindings to libzdb, "A small, easy to use Open Source Database Connection Pool Library"
+ *
+ * Copyright (c) 2012, Chris Lowder (http://github.com/clowder)
+ * Released under the MIT License, please see LICENSE
+ */
+
 #ifndef ZDB_CONNECTION
 #define ZDB_CONNECTION
 
--- lib/libzdb/version.rb
+++ lib/libzdb/version.rb
@@ -1,3 +1,9 @@
+# libzdb (http://github.com/clowder/libzdb)
+# Ruby bindings to libzdb, "A small, easy to use Open Source Database Connection Pool Library"
+#
+# Copyright (c) 2012, Chris Lowder (http://github.com/clowder)
+# Released under the MIT License, please see LICENSE
+
 module Libzdb
   VERSION = "0.0.1"
 end
